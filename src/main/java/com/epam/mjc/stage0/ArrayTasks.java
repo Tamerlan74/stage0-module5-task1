26b0069df98af6cd563c7794d8117c6ad74a2dc0
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-09-01 14:23:28 +0600

    do3

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -99,7 +99,21 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        return null;
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                j++;
+            }
+        }
+        int[] result = new int[j];
+        for (int i = 0, m = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                result[m] = arr[i];
+                m++;
+            }
+        }
+        return result;
+
     }
 
     /**
